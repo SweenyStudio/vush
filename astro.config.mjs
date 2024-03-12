3458c344702e21bca6390a8a2a2f52284abdd960
Mac Sweeny <macsweenydev@gmail.com>
2024-03-13 08:48:30 +1000

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import alpinejs from "@astrojs/alpinejs";
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), alpinejs()],
+  output: "server",
+  adapter: vercel()
+});
