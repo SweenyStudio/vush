87bc07a8614c60b90be1c6d3fbd068cd88db031b
Mac Sweeny <macsweenydev@gmail.com>
2024-03-17 19:39:23 +1000

    Add Astro Navbar component and update fonts and colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				"clearface": ["Clearface", "sans-serif"],
+				"karla": ["Karla", "sans-serif"],
+			},
+			colors: {
+				"vushOrange": "#ffc696",
+				"vushYellow": "#ede891",
+				"vushGreen": "#c9e89b",
+				"vushBlack": "#373737",
+			}
+		},
 	},
 	plugins: [],
 }
