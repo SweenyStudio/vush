5b303e53b908ccd3b8fd5171155fef7fed75631c
Mac Sweeny <macsweenydev@gmail.com>
2024-10-27 18:10:27 +1000

    test 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import alpinejs from "@astrojs/alpinejs";
-import cloudflare from "@astrojs/cloudflare";
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
+  integrations: [tailwind(), alpinejs(), vercel()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
